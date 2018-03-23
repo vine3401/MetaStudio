f2d01eb7ea13bacef57ab1c9d176308080e5d150
李正 <lizheng3401@gmail.com>
2018-03-23 22:49:59 +0800

    部署前的准备

--- MetaStudio/settings.py
+++ MetaStudio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=n4p(%m2r10*45xauv)!2uqoop3mp*5f6$8_1bez89qxszs^nn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.metastudio.top']
 
 
 # Application definition
